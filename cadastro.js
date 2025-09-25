71d25075c296dbf18d9639ef8f2e68f4543767c6
edertec <edertec@msn.com>
2025-09-25 14:32:23 -0300

    chamada js

---
+++ cadastro.js
@@ -0,0 +1,11 @@
+
+
+function cadastrar() {
+    // var nome = document.getElementById("nome").value;
+    // var email = document.getElementById("email").value;
+    // var senha = document.getElementById("senha").value;
+    // console.log(nome, email, senha);    
+    console.log("Nome: " + nome);
+    console.log("Email: " + email);
+    console.log("Senha: " + senha);
+}
